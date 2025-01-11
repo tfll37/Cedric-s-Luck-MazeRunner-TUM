165e0c3ea4c31ab828f9e09b4a13053b1b4018a7
Mark <markkonstantinovmk@gmail.com>
2025-01-11 18:02:54 +0100

    fixed the merge issues caused when synchronising with Andrei's animation reworks
    Signed-off-by: Mark <markkonstantinovmk@gmail.com>

--- core/src/de/tum/cit/fop/maze/TilePropMNGR.java
+++ core/src/de/tum/cit/fop/maze/TilePropMNGR.java
@@ -1,20 +1,45 @@
 package de.tum.cit.fop.maze;
 
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-
 
 public class TilePropMNGR {
 
-
-    public static boolean isTileWalkable(TiledMapTileLayer layer, int tileX, int tileY){
-        if (tileX < 0 || tileX >= layer.getWidth() || tileY < 0 || tileY >= layer.getHeight()){
+    public static boolean isTileWalkable(TiledMapTileLayer layer, int tileX, int tileY) {
+        if (tileX < 0 || tileX >= layer.getWidth() || tileY < 0 || tileY >= layer.getHeight()) {
             return false;
         }
+
         TiledMapTileLayer.Cell cell = layer.getCell(tileX, tileY);
-        if (cell != null && cell.getTile() != null){
+        if (cell != null && cell.getTile() != null) {
             Object property = cell.getTile().getProperties().get("Movement");
             return "true".equals(property);
         }
         return false;
+    }
+
+    public static String getTileDescription(TiledMapTileLayer layer, int tileX, int tileY) {
+        if (tileX < 0 || tileX >= layer.getWidth() || tileY < 0 || tileY >= layer.getHeight()) {
+            return null;
+        }
+
+        TiledMapTileLayer.Cell cell = layer.getCell(tileX, tileY);
+        if (cell != null && cell.getTile() != null) {
+            Object property = cell.getTile().getProperties().get("TileDescrip");
+            return property != null ? property.toString() : null;
+        }
+        return null;
+    }
+
+    public static int getTileType(TiledMapTileLayer layer, int tileX, int tileY) {
+        if (tileX < 0 || tileX >= layer.getWidth() || tileY < 0 || tileY >= layer.getHeight()) {
+            return -1;
+        }
+
+        TiledMapTileLayer.Cell cell = layer.getCell(tileX, tileY);
+        if (cell != null && cell.getTile() != null) {
+            return cell.getTile().getId();
+        }
+        return -1;
     }
 }
