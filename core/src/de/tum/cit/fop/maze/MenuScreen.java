f4ccdbce28011c53f6330b7b81d92df2964bc39f
djolejedii <djordje.vidakovic@tum.de>
2025-01-15 12:57:08 +0100

    Updated the menu AND synced the project between the 3 handsome programmers. Fixed the video loading animation. the background is now a video and music plays.

--- core/src/de/tum/cit/fop/maze/MenuScreen.java
+++ core/src/de/tum/cit/fop/maze/MenuScreen.java
@@ -2,51 +2,66 @@ package de.tum.cit.fop.maze;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * The MenuScreen class is responsible for displaying the main menu of the game.
- * It extends the LibGDX Screen class and sets up the UI components for the menu.
+ * It includes an animated background using frames from the menuvid folder and plays background music.
  */
 public class MenuScreen implements Screen {
-    //variables
     private final Stage stage;
     private final MazeRunnerGame game;
+    private final SpriteBatch spriteBatch;
 
+    private Animation<Texture> animation;
+    private float elapsedTime = 0;
+    private Music backgroundMusic;
 
-    /**
-     * CONSTRUCTOR.
-     * <p>
-     * Sets up the camera, viewport, stage, and UI elements.
-     *
-     * @param game The main game class, used to access global resources and methods.
-     */
     public MenuScreen(MazeRunnerGame game) {
         this.game = game;
+        this.spriteBatch = game.getSpriteBatch();
 
-        var camera = new OrthographicCamera();
-    camera.zoom = 1.5f;
-
+        // Set up the camera and viewport
+        var camera = new com.badlogic.gdx.graphics.OrthographicCamera();
         Viewport viewport = new ScreenViewport(camera);
         stage = new Stage(viewport, game.getSpriteBatch());
 
+        // Load animation frames from the menuvid folder
+        Array<Texture> frames = new Array<>();
+        for (int i = 1; i <= 200; i++) { // Assuming 200 frames
+            String framePath = String.format("menuvid/ezgif-frame-%03d.jpg", i);
+            frames.add(new Texture(Gdx.files.internal(framePath)));
+        }
+        animation = new Animation<>(0.1f, frames, Animation.PlayMode.LOOP);
 
+        // Set up the UI
         Table table = new Table();
         table.setFillParent(true);
         stage.addActor(table);
 
         // Add a label as a title
-        table.add(new Label("Diddy party escape", game.getSkin(), "title")).padBottom(80).row();
+        table.add(new Label("Diddy Party Escape", game.getSkin(), "title")).padBottom(80).row();
 
+        // Create and add buttons
+        TextButton goToGameButton = new TextButton("Go To Game", game.getSkin());
+        TextButton testingButton = new TextButton("Testing", game.getSkin());
+        table.add(goToGameButton).width(300).row();
+        table.add(testingButton).width(350).row();
+
+        // Set up button listeners
         // Create level selection buttons
         for (LevelMNGR.LevelInfo level : LevelMNGR.getAvailableLevels()) {
             TextButton levelButton = new TextButton(
@@ -61,91 +76,73 @@ public class MenuScreen implements Screen {
             });
             table.add(levelButton).width(300).padBottom(20).row();
         }
-        TextButton testingButton = new TextButton("Testing", game.getSkin());
 
         table.add(testingButton).width(350).row();
 
+        // Load background music
+        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("music/i must rest here a moment - spiritual brother.mp3"));
+        backgroundMusic.setLooping(true);
+        backgroundMusic.setVolume(0.5f);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // Ensure backgroundTexture is not null
-        if (game.getBackgroundTexture() != null) {
-            game.getSpriteBatch().begin();
-            game.getSpriteBatch().draw(
-                    game.getBackgroundTexture(),
-                    0, 0,
-                    stage.getViewport().getWorldWidth(),
-                    stage.getViewport().getWorldHeight()
-            );
-            game.getSpriteBatch().end();
-        }
+        // Update elapsed time
+        elapsedTime += delta;
 
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        // Get the current frame of the animation
+        Texture currentFrame = animation.getKeyFrame(elapsedTime);
+
+        // Draw the animated background
+        spriteBatch.begin();
+        spriteBatch.draw(
+                currentFrame,
+                0, 0,
+                Gdx.graphics.getWidth(), Gdx.graphics.getHeight()
+        );
+        spriteBatch.end();
+
+        // Draw the UI
+        stage.act(Math.min(delta, 1 / 30f));
         stage.draw();
     }
 
-    private void createLevelButtons() {
-        Table levelTable = new Table();
-        for (LevelMNGR.LevelInfo level : LevelMNGR.getAvailableLevels()) {
-            TextButton levelButton = new TextButton(level.name(), game.getSkin());
-            levelButton.addListener(new ChangeListener() {
-                @Override
-                public void changed(ChangeEvent event, Actor actor) {
-                    game.goToGame(level);  // Need to modify MazeRunnerGame
-                }
-            });
-            levelTable.add(levelButton).pad(10).row();
-        }
-        levelTable.add(levelTable);
-    }
-
-    private void addLevelSelectButtons(Table table) {
-        for (LevelMNGR.LevelInfo level : LevelMNGR.getAvailableLevels()) {
-            TextButton levelBtn = new TextButton(
-                    level.name() + " (" + level.mapSize() + "x" + level.mapSize() + ")",
-                    game.getSkin()
-            );
-            levelBtn.addListener(new ChangeListener() {
-                @Override
-                public void changed(ChangeEvent event, Actor actor) {
-                    game.goToGame(level);
-                }
-            });
-            table.add(levelBtn).width(300).pad(10).row();
-        }
-    }
-
-
     @Override
     public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true); // Update the stage viewport on resize
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
     public void dispose() {
-        // Dispose of the stage when screen is disposed
         stage.dispose();
+        for (Texture frame : animation.getKeyFrames()) {
+            frame.dispose();
+        }
+        if (backgroundMusic != null) {
+            backgroundMusic.dispose();
+        }
     }
 
     @Override
     public void show() {
-        // Set the input processor so the stage can receive input events
         Gdx.input.setInputProcessor(stage);
-    }
-
-    // The following methods are part of the Screen interface but are not used in this screen.
-    @Override
-    public void pause() {
+        if (backgroundMusic != null) {
+            backgroundMusic.play();
+        }
     }
 
     @Override
-    public void resume() {
-    }
+    public void pause() {}
+
+    @Override
+    public void resume() {}
 
     @Override
     public void hide() {
+        if (backgroundMusic != null) {
+            backgroundMusic.stop();
+        }
     }
 }
